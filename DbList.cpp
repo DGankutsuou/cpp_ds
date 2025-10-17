fe30ffe5fbacdb2ae4c13a6495f404477bcd6fe2
Nameless <147317227+DGankutsuou@users.noreply.github.com>
2025-10-17 10:13:51 +0000

    pushed

---
+++ DbList.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "data_structure/DbList.hpp"
+using namespace std;
+
+
+
+int main(void)
+{
+    DbList <int> list;
+    cout << 
+    return (0);
+}
--- data_structure/DbList.hpp
+++ data_structure/DbList.hpp
@@ -7,17 +7,52 @@ class DbList
 {
 private:
 	class	Node;
-	Node	*_head;
+	Node	*_head = NULL;
 public:
 	class Node
 	{
 	public:
 		T		value;
-		Node	*previous;
-		Node	*next;
+		Node	*previous = NULL;
+		Node	*next = NULL;
 	}
 	DbList(void);
 	~DbList();
+
+	void dblistadd_front(T value)
+	{
+		if (_head == NULL)
+		{
+			_head = new Node;
+			_head->value = T;
+		}
+		else if (_head->previous == NULL)
+		{
+			_head->previous = new Node;
+			_head->previous->value = T;
+			_head->previous->next = _head;
+			_head = _head->previous;
+		}
+		else
+		{
+			_head->previous->next = new Node;
+			_head->previous->next->value = T;
+			_head->previous->next->next = _head;
+			_head->previous->next->previous = _head->previous;
+		}
+	}
+
+	Node *find(T value)
+	{
+		Node *lst = _head;
+		while (lst)
+		{
+			if (lst->value == value)
+				return lst;
+			lst = lst->next;
+		}
+		return NULL;
+	}
 };
 
 DbList<void>::DbList(void)
@@ -27,4 +62,3 @@ DbList<void>::DbList(void)
 DbList<void>::~DbList()
 {
 }
-
