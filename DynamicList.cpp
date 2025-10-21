c056f48e359813ad8f615e02fec37ef7d9171c81
Nameless <147317227+DGankutsuou@users.noreply.github.com>
2025-10-21 14:04:44 +0000

    63%

--- DynamicList.cpp
+++ DynamicList.cpp
@@ -7,8 +7,16 @@ int main(void)
     list.update_item(0, 21);
     list.update_item(1, 42);
     list.update_item(2, 1337);
+    list.reverse();
+    list.print();
+    list.reverse();
+    cout << "the size: " << list.size() << endl;
+    cout << "at 1: " << list.at(1) << endl;
+    list.resize(2);
     list.print();
     cout << "the size: " << list.size() << endl;
     cout << "at 1: " << list.at(1) << endl;
+    list.clear();
+    cout << "the size: " << list.size() << endl;
     return (0);
 }
--- data_structures/DynamicList.hpp
+++ data_structures/DynamicList.hpp
@@ -7,20 +7,24 @@ template <typename T>
 class DynamicList
 {
 protected:
-    T *_array;
+    T *_array{NULL};
     int _size = 0;
 public:
     DynamicList(int s)
     {
         if (s < 0)
             s = 0;
-        _size = s;
-        _array = new T[s];
+        else
+        {
+            _size = s;
+            _array = new T[s];
+        }
     }
 
     ~DynamicList()
     {
         delete[] _array;
+        _array = NULL;
     }
 
     void print(void)
@@ -51,5 +55,49 @@ public:
         if (index < 0 || index >= _size)
             return ;
         _array[index] = value;
+    }
+
+    bool is_empty(void)
+    {
+        return (!_size);
+    }
+
+    void resize(int size)
+    {
+        if(size <= 0)
+        {
+            delete[] _array;
+            _array = NULL;
+            _size = 0;
+        }
+        else
+        {
+            T *new_arr = new T[size];
+            for (int i = 0; i < size && i < _size; i++)
+            {
+                new_arr[i] = _array[i];
+            }
+            delete[] _array;
+            _array = new_arr;
+            _size = size;
+        }
+    }
+
+    void clear(void)
+    {
+        delete[] _array;
+        _array = NULL;
+        _size = 0;
+    }
+
+    void reverse(void)
+    {
+        T *new_arr = new T[_size];
+        for (int i = 0; i < _size; i++)
+        {
+            new_arr[_size - 1 - i] = _array[i];
+        }
+        delete[] _array;  
+        _array = new_arr; 
     }
 };
